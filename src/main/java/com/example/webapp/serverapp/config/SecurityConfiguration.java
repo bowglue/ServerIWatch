e6812e013f08c476de1797d75370e303e5c2a7e8
Yacine Djerroud <80024864+bowglue@users.noreply.github.com>
2022-07-14 20:41:34 -0400

    heroku

--- src/main/java/com/example/webapp/serverapp/config/SecurityConfiguration.java
+++ src/main/java/com/example/webapp/serverapp/config/SecurityConfiguration.java
@@ -43,6 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        http.csrf().disable()
                 .authorizeRequests()
+               .antMatchers("/api/v1").permitAll()
                 .antMatchers("/api/v1/login").permitAll()
                 .antMatchers("/api/v1/register").permitAll()
                 .antMatchers("/api/v1/movie/image").permitAll()
